a9f6c3992437f9ceda55a08dad3a04aac1b66017
erinfoege <erinfoege15@gmail.com>
2025-10-15 19:35:15 -0400

    Start of login

---
+++ app/components/login-screen.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
+
+
+export default function LoginScreen() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>BudgetGator Login</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title="Login" onPress={handleLogin} />
+      {/* <Text>Edit app/index.tsx to edit this screen.</Text> */}
+    </View>
+  );
+}
