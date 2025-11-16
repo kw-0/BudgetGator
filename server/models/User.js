3bd09f9b8ba2aa232926b3b6316114dee9d13b0c
Emma Min Baumgartner <146688511+emmabaumgartner@users.noreply.github.com>
2025-11-16 14:28:08 -0500

    re-add benefactor check

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,8 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isPrimaryUser: { type: Boolean, required: true },
+
 
   plaidAccessToken: { type: String, default: null },
   plaidItemId: { type: String, default: null },
