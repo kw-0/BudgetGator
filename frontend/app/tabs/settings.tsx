740e3af1cf4662cc87f163e4956575360e2053d7
Anushka Kapoor <a.kapoor@ufl.edu>
2025-11-16 17:17:13 -0500

    specific to role bottom bar

--- frontend/app/tabs/settings.tsx
+++ frontend/app/tabs/settings.tsx
@@ -2,12 +2,25 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Settings() {
+
+  const isPrimaryUser = false; // Placeholder for actual user role check
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>hi this is settings</Text>
+//     </View>
+//   );
+// }
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>hi this is settings</Text>
-    </View>
-  );
-}
+      <View style={styles.container}>
+        {isPrimaryUser ? (
+          <Text style={styles.text}>This is settings for PRIMARY users</Text>
+        ) : (
+          <Text style={styles.text}>This is settings for BENEFACTORS</Text>
+        )}
+      </View>
+    );
+  }
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#f9f9f9" },
